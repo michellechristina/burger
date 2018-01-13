ac3d618ddb6dd0db466226b118f0fbf6abb4febb
michellechristina <michelle@thisisbam.com>
2018-01-13 18:33:46 -0500

    db setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+
